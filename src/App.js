74af45b485ca181bf503c7e3b889a27438a33e5c
Sebastian Reyes <sreyes92@csu.fullerton.edu>
2022-09-28 19:57:50 -0700

    Footer Created

--- src/App.js
+++ src/App.js
@@ -5,18 +5,25 @@ import About from './pages/About';
 import Professional from './pages/Professional';
 import Casual from './pages/Casual';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Services from "./pages/Services";
+import ContactUs from "./pages/ContactUs";
+import Footer from "./Footer";
 
 function App(){
     return(
         <Router>
             <div className="App">
                 <Nav />
+                
                 <Routes>
                     <Route path="/" element={<Home/>} />
                     <Route path="/professional" element={<Professional/>} />
                     <Route path="/casual" element={<Casual/>} />
                     <Route path="/about" element={<About/>} />
-                </Routes>
+                    <Route path="/Services" element={<Services/>} />
+                    <Route path="/Contact Us" element={<ContactUs/>} />
+                </Routes> 
+                <Footer />
             </div>
         </Router>
     );
