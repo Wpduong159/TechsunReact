2e4a4ea4118da4612904e2ddb2a663297ddf0968
Sebastian Reyes <sreyes92@csu.fullerton.edu>
2022-10-06 15:34:33 -0700

    Setup Partial Contact Us
    Added image, and styling for contact

--- src/images/imageIndex.js
+++ src/images/imageIndex.js
@@ -1,3 +1,4 @@
 export const City = require('./chicago_night.gif');
 export const ProfSample = require('./prof_sample.jpg');
 export const CasSample = require('./cas_sample.jpg');
+export const Skyward = require('./Skyward.jpg');
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,10 +1,15 @@
 import React from "react";
 import '../App.css';
+import {Skyward} from '../images/imageIndex';
 
 function ContactUs(){
     return(
         <div>
-            <h1>Contact Us</h1>
+
+            <img className = "skyward" src={Skyward} alt = "Skyline background" />
+            <div className="contactTitle">
+                <h1>Contact Us</h1>
+            </div>
         </div>
     );
 }
