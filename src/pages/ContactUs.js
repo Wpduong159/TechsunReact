84527825ecbff39b1459cc1854673be8cc5986ef
Sebastian Reyes <sreyes92@csu.fullerton.edu>
2022-10-08 09:54:30 -0700

    Added image to services Pg

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -8,7 +8,9 @@ function ContactUs(){
 
             <img className = "skyward" src={Skyward} alt = "Skyline background" />
             <div className="contactTitle">
-                <h1>Contact Us</h1>
+                <h1>Contact Us:</h1>
+                <h3>techsunengineering@google.com</h3>
+                
             </div>
         </div>
     );
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,10 +1,14 @@
 import React from "react";
 import '../App.css';
+import { Office } from "../images/imageIndex";
 
 function Services(){
     return(
         <div>
-            <h1>Services Page</h1>
+            <img className = "office" src={Office} alt = "Office backgroud" />
+            <div className="servicesTitle">
+                <h1>Services Page</h1>
+            </div>
         </div>
     );
 }
