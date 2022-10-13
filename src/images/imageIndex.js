e7651ac242ccefa422a18ca94fe96c227298334a
Sebastian Reyes <sreyes92@csu.fullerton.edu>
2022-10-13 09:20:24 -0700

    Slight changes made will discuss in class

--- src/images/imageIndex.js
+++ src/images/imageIndex.js
@@ -4,3 +4,5 @@ export const CasSample = require('./cas_sample.jpg');
 export const Skyward = require('./Skyward.jpg');
 export const Office = require('./office.jpg');
 export const Clothing = require('./clothing.jpg');
+export const Tanjiro = require('./tanjiro.jpg');
+export const Whatif = require('./whatif.jpg');
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,13 +1,23 @@
 import React from "react";
 import '../App.css';
-import { Office } from "../images/imageIndex";
+import { Office, Tanjiro, Whatif } from "../images/imageIndex";
 
 function Services(){
     return(
         <div>
             <img className = "office" src={Office} alt = "Office backgroud" />
             <div className="servicesTitle">
-                 <h1>Services Page</h1>
+                <h1>Services Page</h1>
+                <div className="sideBySide">
+                    <div class='row'>
+                        <div class='column'>
+                            <img src={Whatif} alt = "The matrix scene"/>
+                        </div>
+                        <div class = 'column'>
+                            <img src={Tanjiro} alt = "Demon Slayer anime"/>
+                        </div>
+                    </div>
+                </div>    
             </div>
         </div>
     );
