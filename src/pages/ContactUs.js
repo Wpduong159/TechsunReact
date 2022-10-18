acb9941b3c56177bba21388559c0d49e4facb24e
Sebastian Reyes <sreyes92@csu.fullerton.edu>
2022-10-18 15:44:29 -0700

    New layout wanted

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -5,13 +5,19 @@ import {Skyward} from '../images/imageIndex';
 function ContactUs(){
     return(
         <div>
+            <section className= "top">
+                <img className = "skyward" src={Skyward} alt = "Skyline background" />
+                <div className="contactTitle">
+                    <h1>Contact Us</h1>
+                </div>
+            </section>
+            <section className="emailSection">
+                <h2> Business Inquiries: engineering.techsun@gmail.com</h2>
+                <h3>wpduong@csu.fullerton.edu <br></br>
+                    sreyes92@csu.fullerton.edu
+                </h3>
 
-            <img className = "skyward" src={Skyward} alt = "Skyline background" />
-            <div className="contactTitle">
-                <h1>Contact Us:</h1>
-                <h2> Business Inqueries: engineering.techsun@gmail.com</h2>
-                                               
-            </div>
+            </section>
         </div>
     );
 }
--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,6 +1,13 @@
 import React from "react";
 import '../App.css';
+import Stack from "@mui/material/Stack";
+import Item from "@mui/material/ListItem";
 import { Office, Tanjiro, Whatif } from "../images/imageIndex";
+
+
+
+
+
 
 function Services(){
     return(
@@ -8,16 +15,10 @@ function Services(){
             <img className = "office" src={Office} alt = "Office backgroud" />
             <div className="servicesTitle">
                 <h1>Services Page</h1>
-                <div className="sideBySide">
-                    <div class='row'>
-                        <div class='column'>
-                            <img src={Whatif} alt = "The matrix scene"/>
-                        </div>
-                        <div class = 'column'>
-                            <img src={Tanjiro} alt = "Demon Slayer anime"/>
-                        </div>
-                    </div>
-                </div>    
+                <Stack direction='row' spacing={12}>
+                    <Item md><img className = "tanjiro" src={Tanjiro} alt = "Demon Slayer Anime" /></Item>
+                    <Item md><img className = "whatif" src={Whatif} alt = "Meme" /></Item>
+                </Stack>
             </div>
         </div>
     );
