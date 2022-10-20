083d91bdbab7efc84de8f821d67842776f60604a
Sebastian Reyes <sreyes92@csu.fullerton.edu>
2022-10-19 19:18:45 -0700

    Pushing work done on about uis

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,17 +3,24 @@ import '../App.css';
 import {Clothing} from '../images/imageIndex'
 function About(){
     return(
-        <div>
+        <div> 
+            <section className="top">
             <img className="clothing" src={Clothing} alt = "Clothing rack" />
             <div className="aboutUs">
                 <h1>About Us</h1>
-                <h3>Two Cal State Fullerton Univserity Students with a vision.<br></br>
+                
+            </div>
+            </section>
+            <section className="bottom">
+            <div className="aboutUsDiscription">
+                <p>Two Cal State Fullerton Univserity Students with a vision.<br></br>
                 William Duong and Sebastian Reyes, attempting to create a web application<br></br>
                 that can help insipire individuals find their look for a day in the office.<br></br>
                 Whether it is business or business casual our application can make <br></br>
                 it easier for you to tackle the day!
-                </h3>
-            </div>    
+                </p>
+            </div>
+            </section>   
         </div>
     );
 }
