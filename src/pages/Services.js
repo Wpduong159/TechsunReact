d49169c5880fed9ad3c9a8d7de134bc68dc17292
Sebastian Reyes <sreyes92@csu.fullerton.edu>
2022-10-27 10:04:03 -0700

    Update Services.js

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,9 +1,9 @@
 import React from "react";
 import '../App.css';
-import Stack from "@mui/material/Stack";
-import Item from "@mui/material/ListItem";
-import { Office, Tanjiro, Whatif } from "../images/imageIndex";
 
+import { Office, Tanjiro, Whatif } from "../images/imageIndex";
+import Grid from '@mui/material/Grid'; // Grid version 1
+import Item from '@mui/material/ListItem';
 
 
 
@@ -12,15 +12,27 @@ import { Office, Tanjiro, Whatif } from "../images/imageIndex";
 function Services(){
     return(
         <div>
+            <section className="top">
             <img className = "office" src={Office} alt = "Office backgroud" />
             <div className="servicesTitle">
                 <h1>Services Page</h1>
-                <Stack direction='row' spacing={12}>
-                    <Item md><img className = "tanjiro" src={Tanjiro} alt = "Demon Slayer Anime" /></Item>
-                    <Item md><img className = "whatif" src={Whatif} alt = "Meme" /></Item>
-                </Stack>
             </div>
+            </section>
+            <section className="bottom">
+                <Grid container direction="row" justifyContent='center' alignItems='center'>
+                    <Grid item md>
+                        <Item>Help Desk: helptechsun@gmail.com </Item>
+                    </Grid>
+                    <Grid item md>
+                        <Item>Comments/Concerns: consernstechsun@gmail.com</Item>
+                    </Grid>
+                    <Grid item md>
+                        <Item>Tips: fixthistechsun@gmail.com</Item>
+                    </Grid> 
+                </Grid>
+            </section>
         </div>
+        
     );
 }
 
